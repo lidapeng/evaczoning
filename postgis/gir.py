1631820b5e573ead76c4d6c64141068f44ad359b
Dapeng Li <edward.1985@163.com>
2016-03-25 22:25:58 -0600

    application

--- postgis/gir.py
+++ postgis/gir.py
@@ -4,6 +4,21 @@ Created on Mar 25, 2016
 @author: Dapeng
 '''
 from reversegeocoders.agrclib import AGRCResult,AGRCReverseGeocoder 
-
+from reversegeocoders.geonameslib import GeonamesReverseGeocoder
+from reversegeocoders.openstreetmaplib import OpenStreetResult,OpenStreetReverseGeocoder
+from reversegeocoders.googlelib import GoogleResult,GoogleReverseGeocoder
+from reversegeocoders.distance import VincentyDistance
 if __name__ == '__main__':
+    #40.766453, -111.865761
+    geonamesRGeocoder = GeonamesReverseGeocoder()
+    osmRGeocoder = OpenStreetReverseGeocoder()
+    result = geonamesRGeocoder.reverseGeocode(40.766017, -111.865726)
+    print result.geonameID, result.name, result.distance
+    result1 = osmRGeocoder.reverseGeocode(40.766017, -111.865726)
+    #print result1.displayName
+    googleRGeocoder = GoogleReverseGeocoder()
+    result2 = googleRGeocoder.reverseGeocode(40.766017, -111.865726)
+    print result2.name, result2.latitude, result2.longitude
+    dist = VincentyDistance()
+    print dist.measure([40.766017, -111.865726], [result2.latitude, result2.longitude])
     pass
