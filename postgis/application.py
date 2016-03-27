34c53f4e337bcc2e6deb142d4c69d19585afb05f
Dapeng Li <edward.1985@163.com>
2016-03-27 00:48:25 -0600

    add new files for database manipulation

---
+++ postgis/application.py
@@ -0,0 +1,8 @@
+'''
+Created on Mar 27, 2016
+
+@author: edward
+'''
+
+if __name__ == '__main__':
+    pass
---
+++ postgis/settings.py
@@ -0,0 +1,6 @@
+'''
+Created on Mar 27, 2016
+Settings for database manipulation
+@author: edward
+'''
+
