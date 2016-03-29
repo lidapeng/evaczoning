59b03a8f5c7c968ff682effe399cf3121da4c3d2
Dapeng Li <edward.1985@163.com>
2016-03-28 23:42:54 -0600

    add class

---
+++ gir/querypoints.py
@@ -0,0 +1,17 @@
+'''
+Created on Mar 28, 2016
+A class to generate query points (latitude and longitude) for reverse geocoding
+@author: Dapeng
+'''
+
+class QueryPoints(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
